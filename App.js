aa526b75cf647e09270e2acbfd0fe7bf20516001
J-eris <erickxt@hotmail.com>
2023-11-27 17:30:14 -0600

    Uplaod proyect react native

--- App.js
+++ App.js
@@ -1,10 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { useState, useEffect } from 'react';
+import Header from './src/components/Header';
+import Timer from './src/components/Timer';
+
+const colors = ["#A2D9CE", "#FD8368", "#D7BDE2"]
 
 export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(30 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" | "BREAK" | "SHORT");
+  const [isActive, setIsActive] = useState(false);
+  
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setTime(time - 1)
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    if (time == 0) {
+      setIsActive(false);
+      setIsWorking((prev) => !prev);
+      setTime(isWorking ? 300 : 1500);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, time])
+  
+
+  function handleStartStop() {
+    setIsActive(!isActive);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={[styles.container, {backgroundColor: colors[currentTime]}]}>
+      <Text style={styles.text} >Cronómetro</Text>
+      <Header 
+      currentTime={currentTime} 
+      setCurrentTime={setCurrentTime} 
+      setTime={setTime} />
+      <Timer time={time} />
+      <TouchableOpacity onPress={handleStartStop} style={styles.button} >
+        <Text style={{color: "white", fontWeight: "bold"}} >{isActive ? "STOP" : "START" }</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +56,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 15,
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 32, 
+    fontWeight: "bold",
+    paddingTop: 30,
+  }, 
+  button: {
+    backgroundColor: "#333333",
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
+    alignItems: "center",
   },
 });
